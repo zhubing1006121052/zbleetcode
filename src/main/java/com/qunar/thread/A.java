91440dd9d9985656104cfa3331220bdb0673b4d6
bing.zhu <bing.zhu@qunar.com>
2019-12-11 19:56:54 +0800

    Semaphore

--- src/main/java/com/qunar/thread/A.java
+++
@@ -1,4 +0,0 @@
-package com.qunar.thread;
-
-public class A {
-}
---
+++ src/main/java/com/qunar/thread/SemaphoreTest.java
@@ -0,0 +1,23 @@
+package com.qunar.thread;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        for (int i = 0; i < 4; i++) {
+            new Thread(()->{
+                try {
+                    semaphore.acquire();//sync.acquireSharedInterruptibly(1);  公平锁，如果没有获取那么排队
+                    //semaphore.tryAcquire();//非公平锁   没有获取到就直接返回false, 不进入AQS队列排队
+                    System.out.println(Thread.currentThread().getName());
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();
+                }
+            }).start();
+        }
+    }
+}
