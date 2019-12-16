94eda13f48aa0dce376de3da90b8c368fb51a27f
bing.zhu <bing.zhu@qunar.com>
2019-12-16 10:07:56 +0800

    Semaphore

--- src/main/java/com/qunar/thread/SemaphoreTest.java
+++ src/main/java/com/qunar/thread/SemaphoreTest.java
@@ -8,8 +8,9 @@ public class SemaphoreTest {
         for (int i = 0; i < 4; i++) {
             new Thread(()->{
                 try {
+                    semaphore.acquireUninterruptibly();
                     semaphore.acquire();//sync.acquireSharedInterruptibly(1);  公平锁，如果没有获取那么排队
-                    //semaphore.tryAcquire();//非公平锁   没有获取到就直接返回false, 不进入AQS队列排队
+                    semaphore.tryAcquire();//return sync.nonfairTryAcquireShared(1) >= 0;非公平锁   没有获取到就直接返回false, 不进入AQS队列排队
                     System.out.println(Thread.currentThread().getName());
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
