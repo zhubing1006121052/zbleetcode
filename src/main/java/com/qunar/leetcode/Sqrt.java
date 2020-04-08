33eaa221dc4b6c500e0d447460263b0bd19920a3
bing.zhu <bing.zhu@qunar.com>
2020-04-08 10:22:39 +0800

    平方根

---
+++ src/main/java/com/qunar/leetcode/Sqrt.java
@@ -0,0 +1,78 @@
+package com.qunar.leetcode;
+
+/**
+ * 求一个数的平方根
+ * @Author bing.zhu
+ * @Date 2020/4/8
+ **/
+public class Sqrt {
+
+    public static void main(String[] args) {
+        for(int i=0;i<200;i++){
+            System.out.println(i+" sqrt is "+sqrt(i));
+        }
+        System.out.println();
+    }
+
+    /**
+     * 求一个数的平方根，二分法
+     *          12
+     *        (0,12)
+     *     (0,6)     (6,12)
+     *  (0,3)  (3,6)
+     *     (3,4)  (4,6)
+     *  (3,3) (3,4)
+     * @param n
+     * @return
+     */
+    public static int sqrt(int n){
+        return sqrt2(0,n,n);
+    }
+
+    /**
+     *
+     * @param left
+     * @param right
+     * @param target
+     * @return
+     */
+    public static int sqrt(int left, int right,int target){
+        if(left == right){
+            return left;
+        }else if(left+1 == right){
+            if(target - left*left < right*right - target){
+                return left;
+            }else{
+                return right;
+            }
+        }
+        int middle = (left + right)/2;
+        if(middle*middle == target){
+            return middle;
+        }
+        if(middle*middle < target){
+            return sqrt(middle,right,target);
+        }else{
+            return sqrt(left,middle,target);
+        }
+    }
+
+    /**
+     * 向上取整
+     * @param left
+     * @param right
+     * @param target
+     * @return
+     */
+    public static int sqrt2(float left, float right,int target){
+
+        float middle = (left + right)/2;
+        if(Math.round(middle*middle) == target){
+            return Math.round(middle);
+        }else if(Math.round(middle*middle) < target){
+            return sqrt2(middle,right,target);
+        }else{
+            return sqrt2(left,middle,target);
+        }
+    }
+}
