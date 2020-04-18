73d15c3c15de44403c571da7c9e808e674e0eb9b
bing.zhu <bing.zhu@qunar.com>
2020-04-18 23:07:21 +0800

    有序链表合并到一个链表中

--- src/main/java/com/qunar/leetcode/MergeTwoArray.java
+++ src/main/java/com/qunar/leetcode/MergeTwoArray.java
@@ -11,8 +11,8 @@ package com.qunar.leetcode;
  * 示例:
  *
  * 输入:
- * nums1 = [1,2,3,0,0,0], m = 3
- * nums2 = [2,5,6],       n = 3
+ * nums1 = [2,5,6,0,0,0], m = 3
+ * nums2 = [1,3,4],       n = 3
  *
  * 输出: [1,2,2,3,5,6]
  *
@@ -22,7 +22,52 @@ package com.qunar.leetcode;
  */
 public class MergeTwoArray {
 
+    public static void mergeaa(int[] nums1, int m, int[] nums2, int n) {
+
+        //尾插法
+        int m1 = m-1,n2 = n-1,m2 = m + n-1;
+        while(m1 >= 0 && n2 >= 0){
+            if(nums1[m1] <= nums2[n2]){
+                nums1[m2--] = nums2[n2--];
+            }else {
+                nums1[m2--] = nums1[m1--];
+            }
+        }
+        while(n2 >= 0){
+            nums1[m2--] = nums2[n2--];
+        }
+    }
+
+    public static int[] aa(int a[], int b[]){
+        int result[] = new int[a.length + b.length];
+        int aindex = 0,bindex = 0,rindex = 0;
+        while(aindex < a.length && bindex < b.length){
+
+            if(a[aindex] < b[bindex]){
+                result[rindex++] = a[aindex++];
+            }else{
+                result[rindex++] = b[bindex++];
+            }
+        }
+        if(rindex < result.length - 1){
+            if(aindex < a.length - 1){
+                while(aindex < a.length){
+                    result[rindex++] = a[aindex++];
+                }
+            }else{
+                while(bindex < b.length){
+                    result[rindex++] = b[bindex++];
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
+
+        int[] ints = {1, 2, 3, 0, 0, 0};
+        mergeaa(ints,3, new int[]{2,5,6},3);
+
 
     }
 
